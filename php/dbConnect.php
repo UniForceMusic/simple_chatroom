8d8318f2ce3b4a48d31a3943784eeeab74e77a6f
UniForceMusic <51342446+UniForceMusic@users.noreply.github.com>
2020-02-20 00:47:30 +0100

    Add files via upload

---
+++ php/dbConnect.php
@@ -0,0 +1,10 @@
+<?php
+    // Login variables
+    $dbServer = "localhost";
+    $dbUsername = "root";
+    $dbPassword = "";
+    $dbName = "chatroom";
+
+    /* Connection variable */
+    $connection = mysqli_connect($dbServer, $dbUsername, $dbPassword, $dbName);
+?>
